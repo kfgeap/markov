31a72dd5125318633aab489e63ae6f5badb0f03d
kfgeap <ootorihazime@gmail.com>
2018-11-14 21:32:07 +0900

    2

--- markovbot.py
+++ markovbot.py
@@ -13,10 +13,10 @@ def markovbot():
 
 def create_oath_session(oath_key_dict):
     oath = OAuth1Session(
-    oath_key_dict['dukxusBDK3V2jFvqeU1hy6Grk'],
-    oath_key_dict['iq5VK8g80KNk8gscXl702CfEu3ELuLTjba0sxXbwBsRkM8VTGX'],
-    oath_key_dict['2351277440-6A7Me8lE80twEaClraU0QKeMPTKtMGRUxg8jlea'],
-    oath_key_dict['uNktGwx2U3FxiidhH1orHOOFcocIxSd2VQ5iRb1X7AUTp']
+    oath_key_dict['-'],
+    oath_key_dict['-'],
+    oath_key_dict['-'],
+    oath_key_dict['-']
     )
     return oath
 
